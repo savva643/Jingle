81e34f4ae1c1f04b1a57bdf25197d590b681edb1
savva643 <savvabartenev@gmail.com>
2024-02-08 20:09:37 +0400

    пп

---
+++ app/src/main/java/com/example/jingle/FirstaStart.java
@@ -0,0 +1,14 @@
+package com.example.jingle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstaStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firsta_start);
+    }
+}
